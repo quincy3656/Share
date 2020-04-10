7b276c498dbabdbb58e290f09ebc86117b250703
quincy3656 <59022964+quincy3656@users.noreply.github.com>
2020-04-10 08:21:42 -0400

    Update MonitorScript.sh
    removed <> from the email line,  bash gives me errors about an unexpected new line when they are there.

--- MonitorScript.sh
+++ MonitorScript.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 SERVER="0.0.0.0"
 ping -c 2 $SERVER &> /dev/null
 if [ $? -ge 1 ]; then
 	echo "Server down at $(date)" >> /var/log/serverdown.log
-	echo "Server down at $(date)" | mailx -s "Server Down" <email@lol.com>
+	echo "Server down at $(date)" | mailx -s "Server Down" email@lol.com
 fi
