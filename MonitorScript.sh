91414d8a60e37e9d055c938e732197e7d553008c
quincy3656 <59022964+quincy3656@users.noreply.github.com>
2020-04-24 15:06:25 -0400

    Update MonitorScript.sh

--- MonitorScript.sh
+++ MonitorScript.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-SERVER="0.0.0.0"
+#Created by Quinton Brown
+
+#Main Variables
+#Cam server:
+#SERVER="127.0.0.1"
+#Gaming PC:
+#SERVER2="127.0.0.1"
+
+#Cam Server ping check:
 ping -c 2 $SERVER &> /dev/null
 if [ $? -ge 1 ]; then
-	echo "Server down at $(date)" >> /var/log/serverdown.log
-	echo "Server down at $(date)" | mailx -s "Server Down" email@lol.com
+	echo "Cam server unreachable at $(date)" >> /var/log/serverdown.log
+	#	echo "Cam server unreachable at $(date)" | mailx -s "Server Down" email@domain.com
+fi
+#Gaming PC ping check:
+ping -c 2 $SERVER2 &> /dev/null
+if [ $? -ge 1 ]; then
+	echo "Gaming computer unreachable at $(date)" >> /var/log/serverdown.log
+	#       echo "Gaming computer unreachable at $(date)" | mailx -s "Server Down" email@domain.com
+
 fi
