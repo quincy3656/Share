6c2d5c0e7475bb9ecd48368019ff32e1a9ccbc6c
quincy3656 <59022964+quincy3656@users.noreply.github.com>
2020-04-28 14:00:31 -0400

    Update backupDirectory.sh
    Fixed bug where backing up a directory past root level would throw errors.  Needed to remove the compare files being located in /tmp/.

--- backupDirectory.sh
+++ backupDirectory.sh
@@ -2,7 +2,6 @@
 #Backup a directory if there is a difference from the last backup.
 #You can edit the variables as needed, other than that the script takes care of the rest! This is best pared with a cron entry or cronjob.
 #Created by Quinton Brown 4-24-2020
-#Version 3.0
 
 ###Variables###
 
@@ -18,7 +17,7 @@ LOGS=/var/log/dirBackups.log	#Log location
 
 function compare
 {
-find $dir -type f -exec md5sum {} \; | sort -k 2 | md5sum | cut -b1-33 > /tmp/$dir.md5compare 
+find $dir -type f -exec md5sum {} \; | sort -k 2 | md5sum | cut -b1-33 > $dir.md5compare 
 }
 
 #Main backup function
@@ -30,7 +29,7 @@ if [ $? -ge 1 ]; then
 #	echo "$dir Backup failed at $(date) check logs" | mailx -s "Backups Failed" $email
 else
 	echo "$dir Backup success at $(date)" >> $LOGS
-	cat /tmp/$dir.md5compare > /tmp/$dir.currentmd5
+	cat $dir.md5compare > $dir.currentmd5
 	mv $dir.Backup*.tar.bz2 $STORAGE
 fi
 }
@@ -40,7 +39,7 @@ fi
 function comparemd5sum 
 {
 compare
-diff -q /tmp/$dir.md5compare /tmp/$dir.currentmd5 > /dev/null 2>&1
+diff -q $dir.md5compare $dir.currentmd5 > /dev/null 2>> $LOGS
 if [ $? -ge 1 ]; then
 	echo "$dir md5sum differs, running backup and zipping files" >> $LOGS
 	backup		#Directory contents has changed, do a backup.
@@ -76,19 +75,19 @@ done
 
 #Check if md5tmp is blank first, then execute functions accordingly.
 
-if [ -s /tmp/$dir.currentmd5 ]; then
+if [ -s $dir.currentmd5 ]; then
 	echo "Comparing $dir md5sums and determining if backups are nessisary." >> $LOGS #file is not blank, move on to checking last md5sum.
 	comparemd5sum 
 else
 	#file is blank, this is the first time running, move on to main backup function.
 	echo "Ran first time setup and $dir backup at $(date)" >> $LOGS
-	if [ -s /dirBackups ]; then
+	if [ -d /dirBackups ]; then
 		echo "dirBackups already created, skipping creation and moving on with backup of $dir on $(date)" >> $LOGS
 		compare
 		backup
 	else
 		echo "Creating dirBackups directory for the first time on $(date)" >> $LOGS
-		mkdir $STORAGE
+		mkdir -p $STORAGE
 		compare
 		backup
 	fi	
